c3c0b330d1ce0e66335e45b733e12fa85637c117
Jamie <jamiestobbs92@gmail.com>
2013-09-26 14:45:02 +0800

    update mapbox #4

--- js/index.js
+++ js/index.js
@@ -5,33 +5,20 @@ $(document).ready(function () {
     // Start Mapbox
     var mapdiv = '<div id="map"></div>';
     $('#info .address').replaceWith(mapdiv);
-    var map = L.mapbox.map('map', 'jamesford.map-hcxuhohe', {zoomControl: false}).setView([39.996, 116.332283], 14);
+    var map = L.mapbox.map('map', 'jamesford.map-hcxuhohe', {zoomControl: false, attributionControl: false}).setView([39.996, 116.332283], 14);
 
-    // Do not touch the map!
+    // No interaction
     map.dragging.disable();
     map.touchZoom.disable();
     map.doubleClickZoom.disable();
     map.scrollWheelZoom.disable();
 
-    // Add Marker
-    var markerLayer = L.mapbox.markerLayer({
-        type: 'FeatureCollection',
-        features: [{
-            type: 'Feature',
-            properties: {
-                address: address
-            },
-            geometry: {
-                type: 'Point',
-                coordinates: [116.332283, 39.995533]
-            }
-        }]
-    })
-    .addTo(map);
-
-    // Customize marker & Popup by default
-    markerLayer.eachLayer(function(layer) {
-        var content = '<h3>' + layer.feature.properties.address + '<\/h3>';
-        layer.bindPopup(content).openPopup();
+    // Add location
+    var marker = L.marker(new L.LatLng(39.995533, 116.332283), {
+        icon: L.mapbox.marker.icon(),
+        clickable: false
     });
+    marker.addTo(map);
+    var content = '<h3>' + address + '<\/h3>';
+    marker.bindPopup(content, {closeButton: false, closeOnClick: false}).openPopup();
 });
